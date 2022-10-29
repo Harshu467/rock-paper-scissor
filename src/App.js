7c793b2629a4e8ac39a8847f48914951baea7083
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-10-29 10:47:27 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-
-import React from "react";
-
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>
-          Hello {name}
-        </h1>
-      </>
-    );
-  }
+import React from 'react'
+import Header from './components/Header'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Game from './components/Game'
+import Footer from './components/Footer'
+import Play from './components/Play'
+const App = () => {
+  return (
+    <>
+      <div className='container'>
+        <Header />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={Play} />
+            <Route path='/game' element={Game} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
